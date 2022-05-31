87705fc9949d54a644764103de59560efe6352b3
rai-kargo <adwitya.paramartha@kargo.tech>
2022-05-31 11:55:43 +0700

    push seadanya csv

--- graph/generated/generated.go
+++ graph/generated/generated.go
@@ -45,8 +45,9 @@ type DirectiveRoot struct {
 
 type ComplexityRoot struct {
 	Mutation struct {
-		SaveShipment func(childComplexity int, id *string, name string, origin string, destination string, deliveryDate string, truckID *string) int
-		SaveTruck    func(childComplexity int, id *string, plateNo string, isdeleted bool) int
+		SaveShipment         func(childComplexity int, id *string, name string, origin string, destination string, deliveryDate string, truckID *string) int
+		SaveTruck            func(childComplexity int, id *string, plateNo string, isdeleted bool) int
+		SendTruckDataToEmail func(childComplexity int, email string) int
 	}
 
 	Query struct {
@@ -73,6 +74,7 @@ type ComplexityRoot struct {
 type MutationResolver interface {
 	SaveTruck(ctx context.Context, id *string, plateNo string, isdeleted bool) (*model.Truck, error)
 	SaveShipment(ctx context.Context, id *string, name string, origin string, destination string, deliveryDate string, truckID *string) (*model.Shipment, error)
+	SendTruckDataToEmail(ctx context.Context, email string) (bool, error)
 }
 type QueryResolver interface {
 	PaginatedTrucks(ctx context.Context, id *string, plateNo *string, page int, first int) ([]*model.Truck, error)
@@ -117,6 +119,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.Mutation.SaveTruck(childComplexity, args["id"].(*string), args["plateNo"].(string), args["isdeleted"].(bool)), true
+
+	case "Mutation.sendTruckDataToEmail":
+		if e.complexity.Mutation.SendTruckDataToEmail == nil {
+			break
+		}
+
+		args, err := ec.field_Mutation_sendTruckDataToEmail_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Mutation.SendTruckDataToEmail(childComplexity, args["email"].(string)), true
 
 	case "Query.paginatedShipments":
 		if e.complexity.Query.PaginatedShipments == nil {
@@ -326,6 +340,8 @@ type Mutation {
    deliveryDate: String!,
    truckId: ID,
   ): Shipment!
+
+ sendTruckDataToEmail(email: String!): Boolean!
 }
 `, BuiltIn: false},
 }
@@ -425,6 +441,21 @@ func (ec *executionContext) field_Mutation_saveTruck_args(ctx context.Context, r
 		}
 	}
 	args["isdeleted"] = arg2
+	return args, nil
+}
+
+func (ec *executionContext) field_Mutation_sendTruckDataToEmail_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 string
+	if tmp, ok := rawArgs["email"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("email"))
+		arg0, err = ec.unmarshalNString2string(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["email"] = arg0
 	return args, nil
 }
 
@@ -718,6 +749,61 @@ func (ec *executionContext) fieldContext_Mutation_saveShipment(ctx context.Conte
 	}()
 	ctx = graphql.WithFieldContext(ctx, fc)
 	if fc.Args, err = ec.field_Mutation_saveShipment_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _Mutation_sendTruckDataToEmail(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Mutation_sendTruckDataToEmail(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Mutation().SendTruckDataToEmail(rctx, fc.Args["email"].(string))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(bool)
+	fc.Result = res
+	return ec.marshalNBoolean2bool(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Mutation_sendTruckDataToEmail(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Mutation",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type Boolean does not have child fields")
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Mutation_sendTruckDataToEmail_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
 		ec.Error(ctx, err)
 		return
 	}
@@ -3202,6 +3288,15 @@ func (ec *executionContext) _Mutation(ctx context.Context, sel ast.SelectionSet)
 
 			out.Values[i] = ec.OperationContext.RootResolverMiddleware(innerCtx, func(ctx context.Context) (res graphql.Marshaler) {
 				return ec._Mutation_saveShipment(ctx, field)
+			})
+
+			if out.Values[i] == graphql.Null {
+				invalids++
+			}
+		case "sendTruckDataToEmail":
+
+			out.Values[i] = ec.OperationContext.RootResolverMiddleware(innerCtx, func(ctx context.Context) (res graphql.Marshaler) {
+				return ec._Mutation_sendTruckDataToEmail(ctx, field)
 			})
 
 			if out.Values[i] == graphql.Null {
--- graph/resolver.go
+++ graph/resolver.go
@@ -1,6 +1,14 @@
 package graph
 
-import "github.com/rai-kargo/kargo-trucks/graph/model"
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rai-kargo/kargo-trucks/graph/model"
+)
 
 // This file will not be regenerated automatically.
 //
@@ -9,4 +17,50 @@ import "github.com/rai-kargo/kargo-trucks/graph/model"
 type Resolver struct {
 	Trucks    []*model.Truck
 	Shipments []*model.Shipment
+}
+
+func (r *Resolver) Init() {
+	for i := 0; i < 20; i++ {
+		truck := &model.Truck{
+			ID:      fmt.Sprintf("TRUCK-%d", len(r.Trucks)+1),
+			PlateNo: fmt.Sprintf("B %d CD", len(r.Trucks)+1),
+		}
+		r.Trucks = append(r.Trucks, truck)
+	}
+}
+
+func generateCSVData(data [][]string) (string, error) {
+	filename := fmt.Sprintf("truck_%d.csv", time.Now().Unix())
+	f, err := os.Create(filename)
+	defer f.Close()
+
+	if err != nil {
+
+		return "", errors.New(fmt.Sprintf("failed to open file %v", err))
+	}
+
+	w := csv.NewWriter(f)
+	err = w.WriteAll(data) // calls Flush internally
+
+	if err != nil {
+		return "", err
+	}
+
+	return filename, nil
+}
+
+func (r *mutationResolver) generateTruckData() [][]string {
+	trucksData := make([][]string, 10)
+
+	for _, truck := range r.Trucks {
+		if len(trucksData) == 10 {
+			break
+		}
+		trucksData = append(trucksData, []string{
+			truck.ID,
+			truck.PlateNo,
+			fmt.Sprintf("%v", truck.Isdeleted),
+		})
+	}
+	return trucksData
 }
